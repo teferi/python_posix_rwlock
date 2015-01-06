44363f664a3d3f175fca86ea312c9a789f84e388
Kirill Zaitsev <k.zaitsev@me.com>
2015-01-06 03:37:14 +0300

    setup.py: additional fields

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
 
 
-setup(name='Python posix rwlock',
-      version='0.0.1',
-      description='Posix rwlock wrapper class',
+setup(name="Python posix rwlock",
+      version="0.0.1",
+      description="Posix rwlock wrapper class",
+
+      author="Zaitsev Kirill",
+      author_email="k.zaitsev@me.com",
+      url="https://github.com/teferi/python_posix_rwlock",
+
+      packages=["posix_rwlock"],
       ext_modules=[
-          Extension('posix_rwlock', sources=['posix_rwlock.c'])
+          Extension("posix_rwlock", sources=["posix_rwlock.c"])
       ])
